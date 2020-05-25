f406f8fab31fabce801224bf3b382fffd62465c3
VBortnyk <viacheslav.bortnyk@gmail.com>
2020-05-25 15:47:51 +0300

    added "Optional" to findByEmail method;

---
+++ src/main/java/com/dev/cinema/dao/impl/UserDaoImpl.java
@@ -0,0 +1,48 @@
+package com.dev.cinema.dao.impl;
+
+import com.dev.cinema.dao.interfaces.UserDao;
+import com.dev.cinema.exceptions.DataProcessingException;
+import com.dev.cinema.lib.Dao;
+import com.dev.cinema.model.User;
+import com.dev.cinema.util.HibernateUtil;
+import java.util.Optional;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class UserDaoImpl implements UserDao {
+    @Override
+    public User add(User user) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+            return user;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Failed to add user", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            String selectQuery = "FROM User WHERE email = :email";
+            Query query = session.createQuery(selectQuery, User.class);
+            query.setParameter("email", email);
+            return Optional.of((User) query.getSingleResult());
+        } catch (Exception e) {
+            throw new DataProcessingException("Failed to find user by email: " + email, e);
+        }
+    }
+}
---
+++ src/main/java/com/dev/cinema/service/impl/UserServiceImpl.java
@@ -0,0 +1,23 @@
+package com.dev.cinema.service.impl;
+
+import com.dev.cinema.dao.interfaces.UserDao;
+import com.dev.cinema.lib.Inject;
+import com.dev.cinema.lib.Service;
+import com.dev.cinema.model.User;
+import com.dev.cinema.service.interfaces.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Inject
+    private UserDao userDao;
+
+    @Override
+    public User add(User user) {
+        return userDao.add(user);
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        return userDao.findByEmail(email).get();
+    }
+}
