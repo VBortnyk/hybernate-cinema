768b147c0deec48b13af37a3481ebdc6314d36df
VBortnyk <viacheslav.bortnyk@gmail.com>
2020-06-10 12:47:26 +0300

    removed redundant dependencies, resolver

--- src/main/java/com/dev/cinema/config/AppConfig.java
+++ src/main/java/com/dev/cinema/config/AppConfig.java
@@ -1,11 +1,5 @@
 package com.dev.cinema.config;
 
-import com.dev.cinema.model.CinemaHall;
-import com.dev.cinema.model.MovieSession;
-import com.dev.cinema.model.Order;
-import com.dev.cinema.model.ShoppingCart;
-import com.dev.cinema.model.Ticket;
-import com.dev.cinema.model.User;
 import java.util.Properties;
 import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
