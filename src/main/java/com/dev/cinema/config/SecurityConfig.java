c36e2a9a2537c4ececb87a2a26e0c9830d9c6a4d
VBortnyk <viacheslav.bortnyk@gmail.com>
2020-06-19 12:17:38 +0300

    configured DB authentication

--- src/main/java/com/dev/cinema/config/SecurityConfig.java
+++ src/main/java/com/dev/cinema/config/SecurityConfig.java
@@ -2,26 +2,36 @@ package com.dev.cinema.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .passwordEncoder(getEncoder())
-                .withUser("user1").password(getEncoder().encode("1")).roles("USER");
+
+    private final UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
     }
 
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(getEncoder());
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http
+                .authorizeRequests()
                 .antMatchers(HttpMethod.POST,"/cinema-halls/**").hasRole("ADMIN")
                 .antMatchers(HttpMethod.GET,"/cinema-halls/**").authenticated()
                 .antMatchers(HttpMethod.POST,"/movie-sessions/**").hasRole("ADMIN")
@@ -34,8 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET,"/").permitAll()
                 .antMatchers(HttpMethod.POST,"/register").permitAll()
                 .and()
-                .formLogin()
-                .permitAll()
+                .formLogin().permitAll()
                 .and()
                 .httpBasic()
                 .and()
--- src/main/java/com/dev/cinema/security/CustomUserDetailsService.java
+++ src/main/java/com/dev/cinema/security/CustomUserDetailsService.java
@@ -2,13 +2,13 @@ package com.dev.cinema.security;
 
 import com.dev.cinema.model.User;
 import com.dev.cinema.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
-    @Autowired
     private final UserService userService;
 
     public CustomUserDetailsService(UserService userService) {
